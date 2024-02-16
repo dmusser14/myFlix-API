ed0e7ec8bd687ab6af327e6e11977ba3bc37bf82
dmusser14 <dmusser28@gmail.com>
2024-02-15 19:55:55 -0500

    removing authentication from movies endpoint

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.get('/', (req, res) => {
 
 // Return list of all movies to user
 
-app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.get('/movies', async (req, res) => {
     await Movies.find()
       .then((movies) => {
         res.status(201).json(movies);
